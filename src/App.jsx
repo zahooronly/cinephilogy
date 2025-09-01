dcb672c3b1321f396f8bacc3922cc74bdc346996
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-01 18:51:42 +0500

    feat: replace AuthWrapper with LoginForm component for improved login handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
-import { AuthWrapper } from "./components/auth/AuthWrapper";
+import { LoginForm } from "./components/auth/login/LoginForm";
 
 function App() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <AuthWrapper headerText="Login here!">
-        <form className="flex flex-col w-full gap-6">
-          <div className="space-y-1.5">
-            <label className="text-sm font-medium" htmlFor="email">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="email@example.com"
-              className="w-full p-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-black transition-all duration-200"
-            />
-          </div>
-          <div className="space-y-1.5">
-            <label className="text-sm font-medium" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder="••••••••"
-              className="w-full p-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-black transition-all duration-200"
-            />
-          </div>
-          <button className="w-full p-2 mt-2 text-sm font-medium text-white bg-black rounded-md hover:bg-black/90 transition-all duration-200 cursor-pointer">
-            Register
-          </button>
-        </form>
-      </AuthWrapper>
+      <LoginForm />
     </main>
   );
 }
--- src/components/auth/AuthWrapper.jsx
+++ src/components/auth/AuthWrapper.jsx
@@ -1,13 +1,13 @@
 export const AuthWrapper = ({ headerText, children, className }) => {
   return (
-    <div className={`flex justify-center items-center flex-col ${className}`}>
+    <div className={`flex w-full justify-center items-center flex-col ${className}`}>
       <h2 className="scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tighter">
         Cinephilogy Auth
       </h2>
       <p className="scroll-m-20 pt-1 text-xl font-semibold tracking-widest">
         {headerText}
       </p>
-      <div>{children}</div>
+      <div className="w-full h-full flex justify-center items-center">{children}</div>
     </div>
   );
 };
