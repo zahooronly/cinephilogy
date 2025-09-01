e9db60c3c28d8de912380778a6cc4bce320f945c
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-01 18:12:17 +0500

    fix: auth wrapper made

---
+++ src/components/auth/AuthWrapper.jsx
@@ -0,0 +1,13 @@
+export const AuthWrapper = ({ headerText, children }) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h2 className="scroll-m-20 border-b pb-1 text-3xl font-semibold tracking-tighter">
+        Cinephilogy Auth
+      </h2>
+      <p className="scroll-m-20 pt-1 text-xl font-semibold tracking-widest">
+        {headerText}
+      </p>
+      <div>{children}</div>
+    </div>
+  );
+};
