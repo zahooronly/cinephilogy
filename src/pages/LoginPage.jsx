ca68db6fed345ebeb2803a410b12b6126b41c6b2
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-02 11:34:22 +0500

    fix: updated .gitignore file with .env and created Login Page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../components/auth/login/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <LoginForm />
+    </main>
+  );
+};
+
+export default LoginPage;
