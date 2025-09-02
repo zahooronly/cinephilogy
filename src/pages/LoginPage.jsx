5da9c6592503e4857ce52a050e59446b659c8046
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-02 11:37:41 +0500

    fix: removed login component and converted directly it into page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,37 @@
-import { LoginForm } from "../components/auth/login/LoginForm";
+import { AuthWrapper } from "../components/auth/AuthWrapper";
 
 const LoginPage = () => {
   return (
     <main className="flex justify-center items-center h-screen">
-      <LoginForm />
+      <AuthWrapper headerText="Login here!" className="m-4">
+        <form className="flex flex-col w-full gap-2 max-w-sm">
+          <div className="space-y-1.5">
+            <label className="text-sm font-medium" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="email@example.com"
+              className="w-full p-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-black transition-all duration-200"
+            />
+          </div>
+          <div className="space-y-1.5">
+            <label className="text-sm font-medium" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="••••••••"
+              className="w-full p-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-black transition-all duration-200"
+            />
+          </div>
+          <button className="w-full p-2 mt-2 text-sm font-medium text-white bg-black rounded-md hover:bg-black/90 transition-all duration-200 cursor-pointer">
+            Login
+          </button>
+        </form>
+      </AuthWrapper>
     </main>
   );
 };
