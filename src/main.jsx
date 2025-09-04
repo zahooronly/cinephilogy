299ef582d1b063277a45050cb817cb3c07b56c87
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-04 12:05:39 +0500

    fix: made routes handle declarative mode way

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 import App from "./App.jsx";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Movies from "./pages/Movies/index.jsx";
-import TV from "./pages/TV/index.jsx";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="movies" element={<Movies />} />
-      <Route path="tv" element={<TV />} />
-    </Route>
-  )
-);
+import Movies from "./pages/Movies";
+import TV from "./pages/TV";
+import Favourite from "./pages/Favourite";
+import About from "./pages/About/index.jsx";
+import Contact from "./pages/Contact/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="tv" element={<TV />} />
+          <Route path="favourite" element={<Favourite />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
