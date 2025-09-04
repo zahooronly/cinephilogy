fbea263ff6f01f01a268b0ad959cb0a66b04f221
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-04 12:05:03 +0500

    fix: updated svg use

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
 import { AuthWrapper } from "../../components/layout/AuthWrapper";
 import { Button } from "../../components/ui/Button";
-import { AUTH_API_URL } from "../../lib/constants";
 import { useNavigate } from "react-router";
 import { getUser } from "../../lib/utils/getUser";
 import { AuthAPI } from "../../services/api";
+import OpenEyeIcon from "../../assets/svgs/open-eye.svg?react";
+import CloseEyeIcon from "../../assets/svgs/close-eye.svg?react";
 
 const Login = () => {
   const navigate = useNavigate();
   const user = getUser();
+
   useEffect(() => {
     if (user) navigate("/");
   }, [user, navigate]);
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
   const [showPassword, setShowPassword] = useState(false);
+
   const loginHandler = (e) => {
     e.preventDefault();
     try {
@@ -32,6 +37,12 @@ const Login = () => {
       console.error("Error:", err);
     }
   };
+
+  const handlePasswordChange = (e) =>
+    setFormData({ ...formData, password: e.target.value });
+  const handleEmailChange = (e) =>
+    setFormData({ ...formData, email: e.target.value });
+
   return (
     <main className="flex justify-center items-center h-screen">
       <AuthWrapper headerText="Login here!" className="m-4">
@@ -45,9 +56,7 @@ const Login = () => {
               id="email"
               placeholder="email@example.com"
               className="w-full p-2 border focus:outline-none focus:ring-1 focus:ring-black transition-all duration-200"
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
+              onChange={handleEmailChange}
             />
           </div>
           <div className="space-y-1.5">
@@ -60,9 +69,7 @@ const Login = () => {
                 id="password"
                 placeholder="••••••••"
                 className="w-full p-2 border focus:outline-none focus:ring-1 focus:ring-black transition-all duration-200"
-                onChange={(e) =>
-                  setFormData({ ...formData, password: e.target.value })
-                }
+                onChange={handlePasswordChange}
               />
               <button
                 type="button"
@@ -70,41 +77,14 @@ const Login = () => {
                 onClick={() => setShowPassword(!showPassword)}
               >
                 {showPassword ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                    <circle cx="12" cy="12" r="3" />
-                  </svg>
+                  <OpenEyeIcon />
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
-                    <line x1="1" y1="1" x2="23" y2="23" />
-                  </svg>
+                  <CloseEyeIcon />
                 )}
               </button>
             </div>
           </div>
-          <Button
-            // className="w-full p-2 mt-2 text-sm font-medium text-white bg-black hover:bg-black/90 transition-all duration-200 cursor-pointer"
-            onClick={loginHandler}
-          >
-            Login
-          </Button>
+          <Button onClick={loginHandler}>Login</Button>
         </form>
       </AuthWrapper>
     </main>
