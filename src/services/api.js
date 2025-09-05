817d5bfdcda8a7d72ef260956b68f34f234fc98e
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-05 10:22:50 +0500

    refactor(api): improve API request structure by using params object

--- src/services/api.js
+++ src/services/api.js
@@ -2,49 +2,80 @@ import axios from "axios";
 import axiosInstance from "./axios";
 import { AUTH_API_URL } from "../lib/constants";
 
-
 export const MoviesAPI = {
-  getAll: async (pagination) => {
-    const response = await axiosInstance.get(
-      `/discover/movie?include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc&page=${pagination}`
-    );
+  getAll: async () => {
+    const response = await axiosInstance.get("/discover/movie", {
+      params: {
+        include_adult: false,
+        include_video: false,
+        language: "en-US",
+        sort_by: "popularity.desc",
+      },
+    });
     return response;
   },
   getMovieDetail: async (id) => {
-    const response = await axiosInstance.get(`/movie/${id}?language=en-US`);
+    const response = await axiosInstance.get(`/movie/${id}`, {
+      params: {
+        language: "en-US",
+      },
+    });
     return response;
   },
   getSearchedMovies: async (query) => {
-    const response = await axiosInstance.get(
-      `/search/movie?include_adult=false&language=en-US&query=${query}`
-    );
+    const response = await axiosInstance.get(`/search/movie`, {
+      params: {
+        include_adult: false,
+        language: "en-US",
+        query: query,
+      },
+    });
     return response;
   },
   getSimilar: async (id) => {
-    const response = await axiosInstance.get(
-      `/movie/${id}/similar?language=en-US`
-    );
+    const response = await axiosInstance.get(`/movie/${id}/similar`, {
+      params: {
+        language: "en-US",
+      },
+    });
     return response;
   },
   getPopular: async () => {
-    const response = await axiosInstance.get("/movie/popular");
+    const response = await axiosInstance.get(`/movie/popular`, {
+      params: {
+        language: "en-US",
+      },
+    });
     return response;
   },
   getTopRated: async () => {
-    const response = await axiosInstance.get("/movie/top_rated");
+    const response = await axiosInstance.get(`/movie/top_rated`, {
+      params: {
+        language: "en-US",
+      },
+    });
     return response;
   },
   getUpcoming: async () => {
-    const response = await axiosInstance.get("/movie/upcoming");
+    const response = await axiosInstance.get(`/movie/upcoming`, {
+      params: {
+        language: "en-US",
+      },
+    });
     return response;
   },
 };
 
 export const TvAPI = {
-  getAll: async (pagination) => {
-    const response = await axiosInstance.get(
-      `/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&sort_by=popularity.desc&page=${pagination}`
-    );
+  getAll: async () => {
+    const response = await axiosInstance.get(`/discover/tv`, {
+      params: {
+        include_adult: false,
+        include_null_first_air_dates: false,
+        language: "en-US",
+        sort_by: "popularity.desc",
+      },
+    });
     return response;
   },
 };
