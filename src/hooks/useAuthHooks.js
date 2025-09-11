96c4e31a3b9208f06d6bbe2f00be63327b89b85c
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-09-11 12:05:20 +0500

    bugfix: auth token sync issue resolved

--- src/hooks/useAuthHooks.js
+++ src/hooks/useAuthHooks.js
@@ -2,6 +2,8 @@ import useAuthStore from "../app/authStore";
 
 export const useAuth = () => {
   const { token } = useAuthStore();
+  const originalToken = import.meta.env.VITE_ACCESS_TOKEN;
   if (!token) return false;
-  return true;
+  if (token == originalToken) return true;
+  else return false;
 };
